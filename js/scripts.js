06f302afaa6d356dcedcb481ee2b327e3ad503b0
Slavcho Mitrov <slavemitrov@yahoo.com>
2018-02-12 17:24:15 +0100

    Add business logic constructor for addresses and html form elements for collecting user addresses and a place to display them

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,40 @@
+//business logic
 function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
+  this.addresses = [];
 };
+
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+};
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+};
+
+//user interface logic
+$(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li");
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+    });
+  });
+});
